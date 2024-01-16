e6d43c788e008725b5ffe5c3be19d62103fcf064
unknown <emersson126@gmail.com>
2024-01-16 09:15:46 -0500

    consultay y guardar diferente 3

--- index.php
+++ index.php
@@ -19,50 +19,55 @@
     </div>
 
     <script>
-        // Mueve la definición de obtenerValor fuera de guardarEnTabla
-        function obtenerValor(objeto, ruta, predeterminado = null) {
-            const propiedades = ruta.split('.');
-            let valor = objeto;
-            for (const propiedad of propiedades) {
-                if (valor && typeof valor === 'object' && propiedad in valor) {
-                    valor = valor[propiedad];
-                } else {
-                    return predeterminado;
-                }
+    // Mueve la definición de obtenerValor fuera de guardarEnTabla
+    function obtenerValor(objeto, ruta, predeterminado = null) {
+        const propiedades = ruta.split('.');
+        let valor = objeto;
+        for (const propiedad of propiedades) {
+            if (valor && typeof valor === 'object' && propiedad in valor) {
+                valor = valor[propiedad];
+            } else {
+                return predeterminado;
             }
-            return valor;
+        }
+        return valor;
+    }
+
+    function consultarSunat() {
+        var ruc = document.getElementById("rucInput").value;
+        
+        // Validar el formato del RUC antes de realizar la consulta
+        if (!/^\d{11}$/.test(ruc)) {
+            alert("Ingrese un número de RUC válido.");
+            return;
         }
 
-        function consultarSunat() {
-            var ruc = document.getElementById("rucInput").value;
-            
-            // Validar el formato del RUC antes de realizar la consulta
-            if (!/^\d{11}$/.test(ruc)) {
-                alert("Ingrese un número de RUC válido.");
-                return;
+        var apiUrl = `https://api.sunat.dev/ruc-premium/${ruc}?apikey=hmWkX8YV2qNsp2keZUW3R4tnb3mqOUWfexjzIucOdvfhnU6pmMJGXcO2RqTWMIQC`;
+
+        $.ajax({
+            url: apiUrl,
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                // La API respondió correctamente
+                console.log(data);
+                guardarEnTabla(data);
+                mostrarResultados(data);
+            },
+            error: function (error) {
+                // La API respondió con error
+                mostrarError(error);
             }
+        });
+    }
 
-            var apiUrl = `https://api.sunat.dev/ruc-premium/${ruc}?apikey=hmWkX8YV2qNsp2keZUW3R4tnb3mqOUWfexjzIucOdvfhnU6pmMJGXcO2RqTWMIQC`;
-
-            $.ajax({
-                url: apiUrl,
-                type: 'GET',
-                dataType: 'json',
-                success: function (data) {
-                    // La API respondió correctamente
-                    console.log(data);
-                    guardarEnTabla(data);
-                    mostrarResultados(data);
-                },
-                error: function (error) {
-                    // La API respondió con error
-                    mostrarError(error);
-                }
-            });
-        }
-
-        function guardarEnTabla(data) {
-            if (data && data.body) {
+    function guardarEnTabla(data) {
+        if (data && data.body) {
+            if (data.statusCode === 400 && data.body.errors && data.body.errors.length > 0) {
+                // Manejar errores específicos de la API
+                var errorMessage = data.body.errors[0].message;
+                console.error(`Error en la respuesta de la API: ${errorMessage}`);
+            } else {
                 $.ajax({
                     url: 'https://nextius.net/RUC/guardarEnTabla.php',
                     type: 'POST',
@@ -98,55 +103,61 @@
                         console.error("Error al guardar en la tabla: " + error.responseText);
                     }
                 });
-            } else {
-                console.error("Error: Estructura de datos incorrecta");
             }
+        } else {
+            console.error("Error: Estructura de datos incorrecta");
         }
+    }
 
-        function mostrarResultados(data) {
-            var resultadosDiv = document.getElementById("resultados");
-            resultadosDiv.innerHTML = `<p>Estado: ${obtenerValor(data, 'statusCode')}</p>`;
-            resultadosDiv.innerHTML += `<p>RUC: ${obtenerValor(data.body, 'numeroRuc')}</p>`;
+    function mostrarResultados(data) {
+        var resultadosDiv = document.getElementById("resultados");
+        
+        // Verificar si hay un error específico en la respuesta
+        if (data.statusCode === 400 && data.body.errors && data.body.errors.length > 0) {
+            var errorMessage = data.body.errors[0].message;
+            resultadosDiv.innerHTML = `<p>Error en la respuesta de la API: ${errorMessage}</p>`;
+        } else {
+            resultadosDiv.innerHTML = `<p>Estado: ${data.statusCode}</p>`;
+            resultadosDiv.innerHTML += `<p>RUC: ${data.body.numeroRuc}</p>`;
             resultadosDiv.innerHTML += `<h1>DATOS DEL CONTRIBUYENTE</h1>`;
-            resultadosDiv.innerHTML += `<p>codDomHabido: ${obtenerValor(data.body.datosContribuyente, 'codDomHabido', '')}</p>`;
+            resultadosDiv.innerHTML += `<p>codDomHabido: ${data.body.datosContribuyente ? data.body.datosContribuyente.codDomHabido : ''}</p>`;
             
-            // Verificar si 'contacto' es nulo antes de acceder a sus propiedades
-            if (data.body.datosContribuyente.contacto) {
+            if (data.body.datosContribuyente && data.body.datosContribuyente.contacto) {
                 resultadosDiv.innerHTML += `<p>CONTACTO</p>`;
-                resultadosDiv.innerHTML += `<p>numTelefono1: ${obtenerValor(data.body.datosContribuyente.contacto, 'numTelefono1', '')}</p>`;
-                resultadosDiv.innerHTML += `<p>numTelefono2: ${obtenerValor(data.body.datosContribuyente.contacto, 'numTelefono2', '')}</p>`;
-                resultadosDiv.innerHTML += `<p>numTelefono3: ${obtenerValor(data.body.datosContribuyente.contacto, 'numTelefono3', '')}</p>`;
+                resultadosDiv.innerHTML += `<p>numTelefono1: ${data.body.datosContribuyente.contacto.numTelefono1 || ''}</p>`;
+                resultadosDiv.innerHTML += `<p>numTelefono2: ${data.body.datosContribuyente.contacto.numTelefono2 || ''}</p>`;
+                resultadosDiv.innerHTML += `<p>numTelefono3: ${data.body.datosContribuyente.contacto.numTelefono3 || ''}</p>`;
             }
 
-            resultadosDiv.innerHTML += `<p>Razón Social: ${obtenerValor(data.body.datosContribuyente, 'desRazonSocial', '')}</p>`;
+            resultadosDiv.innerHTML += `<p>Razón Social: ${data.body.datosContribuyente ? data.body.datosContribuyente.desRazonSocial : ''}</p>`;
             resultadosDiv.innerHTML += `<p>UBIGEO</p>`;
-            resultadosDiv.innerHTML += `<p>codUbigeo: ${obtenerValor(data.body.datosContribuyente.ubigeo, 'codUbigeo', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>desDistrito: ${obtenerValor(data.body.datosContribuyente.ubigeo, 'desDistrito', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>desProvincia: ${obtenerValor(data.body.datosContribuyente.ubigeo, 'desProvincia', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>desDepartamento: ${obtenerValor(data.body.datosContribuyente.ubigeo, 'desDepartamento', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>desDireccion: ${obtenerValor(data.body.datosContribuyente, 'desDireccion', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>desNomApe: ${obtenerValor(data.body.datosContribuyente, 'desNomApe', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>codCorreo2: ${obtenerValor(data.body.datosContribuyente, 'codCorreo2', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>codCorreo1: ${obtenerValor(data.body.datosContribuyente, 'codCorreo1', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>codEstado: ${obtenerValor(data.body.datosContribuyente, 'codEstado', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>nombreComercial: ${obtenerValor(data.body, 'nombreComercial', '')}</p>`;
+            resultadosDiv.innerHTML += `<p>codUbigeo: ${data.body.datosContribuyente && data.body.datosContribuyente.ubigeo ? data.body.datosContribuyente.ubigeo.codUbigeo : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>desDistrito: ${data.body.datosContribuyente && data.body.datosContribuyente.ubigeo ? data.body.datosContribuyente.ubigeo.desDistrito : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>desProvincia: ${data.body.datosContribuyente && data.body.datosContribuyente.ubigeo ? data.body.datosContribuyente.ubigeo.desProvincia : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>desDepartamento: ${data.body.datosContribuyente && data.body.datosContribuyente.ubigeo ? data.body.datosContribuyente.ubigeo.desDepartamento : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>desDireccion: ${data.body.datosContribuyente ? data.body.datosContribuyente.desDireccion : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>desNomApe: ${data.body.datosContribuyente ? data.body.datosContribuyente.desNomApe : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>codCorreo2: ${data.body.datosContribuyente ? data.body.datosContribuyente.codCorreo2 : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>codCorreo1: ${data.body.datosContribuyente ? data.body.datosContribuyente.codCorreo1 : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>codEstado: ${data.body.datosContribuyente ? data.body.datosContribuyente.codEstado : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>nombreComercial: ${data.body ? data.body.nombreComercial : ''}</p>`;
             resultadosDiv.innerHTML += `<p>ACTIVIDAD ECONÓMICA</p>`;
-            resultadosDiv.innerHTML += `<p>Primaria: ${obtenerValor(data.body, 'actividadEconomica.0', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>Secundaria: ${obtenerValor(data.body, 'actividadEconomica.1', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>Emision Electrónica</p>`;
-            resultadosDiv.innerHTML += `<p>Inicio: ${obtenerValor(data.body, 'sistemaEmisionElectronica.0', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>Medio: ${obtenerValor(data.body, 'sistemaEmisionElectronica.1', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>Autorizado: ${obtenerValor(data.body, 'sistemaEmisionElectronica.2', '')}</p>`;
-            resultadosDiv.innerHTML += `<p>Padrones: ${obtenerValor(data.body, 'padrones.0', '')}</p>`;
-            console.log(resultadosDiv);
-            // Agrega más información según tus necesidades
+            resultadosDiv.innerHTML += `<p>Primaria: ${data.body && data.body.actividadEconomica ? data.body.actividadEconomica[0] : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>Secundaria: ${data.body && data.body.actividadEconomica ? data.body.actividadEconomica[1] : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>Emisión Electrónica</p>`;
+            resultadosDiv.innerHTML += `<p>Inicio: ${data.body && data.body.sistemaEmisionElectronica ? data.body.sistemaEmisionElectronica[0] : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>Medio: ${data.body && data.body.sistemaEmisionElectronica ? data.body.sistemaEmisionElectronica[1] : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>Autorizado: ${data.body && data.body.sistemaEmisionElectronica ? data.body.sistemaEmisionElectronica[2] : ''}</p>`;
+            resultadosDiv.innerHTML += `<p>Padrones: ${data.body ? data.body.padrones[0] : ''}</p>`;
         }
 
-        function mostrarError(error) {
-            var resultadosDiv = document.getElementById("resultados");
-            resultadosDiv.innerHTML = `<p>Error al consultar la API: ${error.responseText}</p>`;
-        }
-    </script>
-
+        console.log(resultadosDiv);
+    }
+    function mostrarError(error) {
+    var resultadosDiv = document.getElementById("resultados");
+    resultadosDiv.innerHTML = `<p>Error al consultar la API: ${error.responseText}</p>`;
+    }
+    
+</script>
 </body>
 </html>
