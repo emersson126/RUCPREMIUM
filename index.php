a30d441bdefc3c02adfe1784fefa5d85167f74fd
unknown <emersson126@gmail.com>
2024-01-17 06:38:17 -0500

    fino

--- index.php
+++ index.php
@@ -340,7 +340,6 @@
                 success: function (data) {
                     alert("API Key y correo se han actualizado correctamente.");
                     // Puedes agregar aquí cualquier lógica adicional después de la actualización
-                    reload();
 
                 },
                 error: function (xhr, status, error) {
